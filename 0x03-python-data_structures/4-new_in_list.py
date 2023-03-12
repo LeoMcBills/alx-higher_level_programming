bc17c78f8c6f97c6aea9e0964f7d8062b87a327a
LeoMcBills <leokinyera9@gmail.com>
2023-03-12 06:30:18 -0700

    replace at an index of a list but also keep the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new = []
-    if idx <= (len(new) - 1) and idx >= 0:
-        for i in my_list:
-            new.append(my_list[i])
-
-        new[idx] = element
-        return new
-    else:
-        return new
-
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+    new = [i for i in my_list]
+    new[idx] = element
+    return new
