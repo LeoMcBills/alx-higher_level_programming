bda5e86686d9677a9a1c0a4847f949440ce27b2c
LeoMcBills <leokinyera9@gmail.com>
2023-03-13 12:50:45 -0700

    divide members in list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
+multiples = []
 def divisible_by_2(my_list=[]):
     for i in range(len(my_list)):
-        multiples = []
         if my_list[i] % 2 == 0:
             multiples.append(True)
         else:
